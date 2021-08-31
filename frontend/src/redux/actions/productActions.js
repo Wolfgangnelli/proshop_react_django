9a778ab9d6cd4710bb0bee8ec0016aecf9c3bd84
[AndreaS] <solfa_90@hotmail.it>
2021-08-31 16:41:05 +0200

    call products list into home page with redux

--- frontend/src/redux/actions/productActions.js
+++ frontend/src/redux/actions/productActions.js
@@ -1,25 +1,7 @@
 import { GET_PRODUCTS } from "./actionTypes";
 import axios from "axios";
 
-export const getProducts = () => async (dispatch) => {
-  try {
-    dispatch({
-      type: GET_PRODUCTS,
-    });
-
-    const { data } = await axios.get(`/api/products/`);
-
-    dispatch({
-      type: GET_PRODUCTS,
-      payload: data,
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_PRODUCTS,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
-    });
-  }
-};
+export const getProducts = () => ({
+  type: GET_PRODUCTS,
+  payload: axios.get(`/api/products/`),
+});
