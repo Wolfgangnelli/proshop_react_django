2f0c79ff8e49c4873e73d7cb18972376d2d68db9
[AndreaS] <solfa_90@hotmail.it>
2021-09-01 09:15:53 +0200

    add message & loader component

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-import { Spinner } from "react-bootstrap";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 import { getProducts } from "../redux/actions/productActions";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -18,17 +19,19 @@ function HomeScreen() {
     <div>
       <h1>Latest Products</h1>
       {loading ? (
-        <Spinner animation="border" />
+        <Loader />
       ) : error ? (
         <div className="mt-5">
-          <h3 className="text-danger">
-            Sorry, an Error occurs. <i className="fas fa-bug"></i>
-          </h3>
-          <p>({error})</p>
-          <p className="fw-bold fs-2 text-info">
-            Soon we will restore the service. Try later{" "}
-            <i className="fas fa-hand-scissors" style={{ color: "blue" }}></i>
-          </p>
+          <Message variant="danger">
+            <h3 className="text-danger">
+              Sorry, an Error occurs. <i className="fas fa-bug"></i>
+            </h3>
+            <p>({error})</p>
+            <p className="fw-bold fs-2 text-info">
+              Soon we will restore the service. Try later{" "}
+              <i className="fas fa-hand-scissors" style={{ color: "blue" }}></i>
+            </p>
+          </Message>
         </div>
       ) : (
         <Row>
