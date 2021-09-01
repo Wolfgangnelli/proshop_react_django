c5133084c562ef27e8ad857fbddec61dfc399a03
[AndreaS] <solfa_90@hotmail.it>
2021-09-01 13:05:19 +0200

    cart reducer & action, localStorage

--- frontend/src/redux/actions/actionTypes.js
+++ frontend/src/redux/actions/actionTypes.js
@@ -1,2 +1,7 @@
+/* PRODUCTS - PRODUCT_DETAILS */
 export const GET_PRODUCTS = "GET_ALL_PRODUCTS";
 export const GET_PRODUCT_DETAILS = "GET_PRODUCT_DETAILS";
+
+/* CART */
+export const CART_ADD_ITEM = "CART_ADD_ITEM";
+export const CART_REMOVE_ITEM = "CART_REMOVE_ITEM"; //select the word and ctrl + D
--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import products from "./productsReducer";
 import product from "./productReducer";
+import cart from "./cartReducers";
 
 const reducers = {
   productsList: products,
   productDetails: product,
+  cart,
 };
 
 export default combineReducers(reducers);
