e749a79aa5c488eae5323c03823d44bf955d37eb
[AndreaS] <solfa_90@hotmail.it>
2021-09-01 12:00:30 +0200

    qty select & add to cart

--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import ProductPage from "../pages/ProductPage";
+import CartPage from "../pages/CartPage";
 
 function Routes() {
   return (
     <>
       <Route path="/" component={HomePage} exact />
       <Route path="/product/:id" component={ProductPage} />
+      <Route path="/cart/:id?" component={CartPage} />
     </>
   );
 }
