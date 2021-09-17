6041d09602a28c1053884521e0ace9684cb670e5
AndreaS <andrea.solfanelli90@gmail.com>
2021-09-17 10:58:21 +0200

    add create & update products endpoint & views

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from base.models import Product
 from base.serializers import ProductSerializer
-from rest_framework import status
+from rest_framework import serializers, status
 
 
 
@@ -26,3 +26,40 @@ def deleteProduct(request, pk):
     product = Product.objects.get(_id=pk)
     product.delete()
     return Response('Product deleted')
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createProduct(request):
+    user = request.user
+
+    product = Product.objects.create(
+        user = user,
+        name = 'Sample Name',
+        price = 0,
+        brand = 'Sample Brand',
+        countInStock = 0,
+        category = 'Sample Category',
+        description = ''
+    )
+
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateProduct(request, pk):
+    data = request.data
+
+    product = Product.objects.get(_id=pk)
+
+    product.name = data['name'],
+    product.brand = data['brand'],
+    product.category = data['category'],
+    product.description = data['description'],
+    product.price = data['price'],
+    product.countInStock = data['countInStock']
+
+    product.save()
+    serializer = ProductSerializer(product, many=False)
+    Response(serializer.data)
+
